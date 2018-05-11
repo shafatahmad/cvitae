6728e3773dce650bc69f7c27f7b3fb16b909a3ea
shafatahmad <shafat.ahmad@live.com>
2018-05-11 12:36:08 +0530

    reverted back to FA CDN

--- sw.js
+++ sw.js
@@ -4,6 +4,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox
 const staticAssets =[
     './',
     './assets/css/bootstrap4.1.1.min.css',
+    './assets/css/style.min.css',
     './img/avatar.png'
 ];
 

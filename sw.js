60e5bc7573bcb48a9df31864faed046ad38be5be
shafatahmad <shafat.ahmad@live.com>
2018-05-11 11:52:31 +0530

    Inserted Font Awesome SVG with JS
    Use Font Awesome CDN to a deliver cached version of Font Awesome's JS with all of its features enabled to your project.

--- sw.js
+++ sw.js
@@ -3,11 +3,11 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox
 
 const staticAssets =[
     './',
-    './assets/css/style.min.css',
-    './assets/css/script.min.js',
-    './app.js',
+    './assets/css/bootstrap4.1.1.min.css',
     './img/avatar.jpg'
 ];
+
+
 
 if (workbox) {
   console.log('Yay! Workbox is loaded 🎉');
@@ -27,7 +27,7 @@ workbox.routing.registerRoute('https://use.fontawesome.com/releases/v5.0.12/css/
 
 workbox.routing.registerRoute(/\.(?:png|gif|jpg)$/,
 workbox.strategies.cacheFirst({
-    cacheName: 'images',
+    cacheName: 'images-cache',
     cacheExpiration: {
     maxEntries: 50, maxAgeSeconds: 12*60*60
     }})
